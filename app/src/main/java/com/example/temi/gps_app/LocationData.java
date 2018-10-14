5a1a650a80daf9f1e36dfb0644f22e965ee6246a
TemiAkinbo <temi.akinbo01@gmail.com>
2018-10-14 11:48:05 +0100

    first commit for Android Workshop App

---
+++ app/src/main/java/com/example/temi/gps_app/LocationData.java
@@ -0,0 +1,17 @@
+package com.example.temi.gps_app;
+
+public class LocationData {
+    public double latitude;
+    public double longitude;
+
+    public LocationData(){
+
+    }
+
+    public LocationData(double latitude, double longitude){
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+
+}
